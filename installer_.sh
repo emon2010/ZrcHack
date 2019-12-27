f93e01da5f9dd0b67758ed96683079074e89fc63
emon2010 <59183277+emon2010@users.noreply.github.com>
2019-12-27 09:33:35 +0900

    Update installer_.sh

--- installer_.sh
+++ installer_.sh
@@ -1,20 +1,36 @@
-#!/data/data/com.termux/files/usr/bin/bash
+#!/data/data/com.termux/files/usr/bin/bash env
 
+########################################
+#  TCA CLI Environment Setup Script: 1.2
+########################################
+#
+#  Author: The Illusionist <T.me/Theillusionist_TCA> and Josh <T.Me/@IAmJoshBrolin>
+#
+########################################
+#
+# Dependencies: bash and curl.
+#
+########################################
+
+# Update and upgrade
 pkg up -y && pkg upgrade -y
 
+# Switch permissive
 su -c 'setenforce 0'
 
+# Install dependencies
 time apt install rsync aapt neofetch toilet ncurses-utils tsu openssl-tool ruby wget -y
 
+# Remove existing files
 tsu -c 'find . -iname '*scrz*' -exec rm -rf {} \;'
 
+# Device architecture
 [[ "$(uname -m)" =~ 'aarch64' ]] || {
     printf "Your device's architecture isn't officially supported yet."
     exit 1
 }
 
-tsu -c 'wget https://raw.githubusercontent.com/xscrprog/ZrcHack/master/allarchi/scrz_v2.1 -O ./scrz_v2.1'
-tsu -c 'chmod a+x scrz_v2.1'
-tsu -c ./scrz_v2.1
-
-echo "tsudo ./scrz_v2.1 --rc" >> /data/data/com.termux/files/usr/etc/bash.bashrc
+# Fetch the ELF and setup
+tsu -c 'wget https://raw.githubusercontent.com/xscrprog/ZrcHack/master/allarchi/scrz_cli -O ./scrz_cli'
+tsu -c 'chmod a+x scrz_cli'
+tsu -c ./srcz_cli
